dd394eb6d8cfb5dceafcc8d52b8ff0d0de5ad264
Alexander <continenttal2015@yandex.ru>
2023-11-11 05:20:51 +0300

    Added changes to the notes application, added options 5 and 6

--- NotesApplication.py
+++ NotesApplication.py
@@ -51,14 +51,30 @@ class NotesApp:
         notes = [note for note in notes if note['note_id'] != note_id]
         self.save_notes(notes)
 
+    def view_note(self, note_id):
+        notes = self.load_notes()
+        for note in notes:
+            if note['note_id'] == note_id:
+                print(f"ID: {note['note_id']}, Title: {note['title']}, Body: {note['body']}, Timestamp: {note['timestamp']}")
+                return
+        print("Note not found.")
+
+    def filter_notes_by_date(self, start_date, end_date):
+        notes = self.load_notes()
+        filtered_notes = [note for note in notes if start_date <= note['timestamp'] <= end_date]
+        for note in filtered_notes:
+            print(f"ID: {note['note_id']}, Title: {note['title']}, Body: {note['body']}, Timestamp: {note['timestamp']}")
+
 if __name__ == "__main__":
     app = NotesApp('notes.json')
     while True:
         print("1. Добавить заметку")
-        print("2. Читать заметку")
+        print("2. Читать заметки")
         print("3. Обновить заметку")
         print("4. Удалить заметку")
-        print("5. Выход")
+        print("5. Посмотреть заметку")
+        print("6. Фильтровать заметки по дате")
+        print("7. Выход")
         choice = input("Enter your choice: ")
         if choice == "1":
             note_id = input("Enter note ID: ")
@@ -76,6 +92,13 @@ if __name__ == "__main__":
             note_id = input("Enter note ID to delete: ")
             app.delete_note(note_id)
         elif choice == "5":
+            note_id = input("Enter note ID to view: ")
+            app.view_note(note_id)
+        elif choice == "6":
+            start_date = input("Enter start date (YYYY-MM-DD): ")
+            end_date = input("Enter end date (YYYY-MM-DD): ")
+            app.filter_notes_by_date(start_date, end_date)
+        elif choice == "7":
             break
         else:
             print("Invalid choice. Please try again.")
